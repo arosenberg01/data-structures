5e840f54371661da058f2d1c0833742d2fc9fb20
arosenberg01 <ansel01@gmail.com>
2015-03-25 16:03:01 -0700

    completed functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,34 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  // but try not not reference your old code in writing the new styl
+  var queue = {};
+  queue.len = 0;
+  _.extend(queue, queueMethods);
+  return queue;
 };
 
 var queueMethods = {};
+queueMethods.enqueue = function(value){
+  this[this.len] = value;
 
+  this.len++;
+
+}
+queueMethods.dequeue = function(){
+  if (this.len > 0) {
+    var result = this[0];
+    for (var i = 0; i < this.len; i++) {
+      var j = i + 1;
+      this[i] = this[j];
+    }
+    delete this[this.len];
+    this.len--;
+  }
+  return result;
+}
+queueMethods.size = function(){
+  console.log(this.len);
+  return this.len;
+}
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.len = 0;
+
+  _.extend(stack, stackMethods)
+  return stack;
 };
 
-var stackMethods = {};
 
+var stackMethods = {};
+stackMethods.push = function(value){
+  this.len++;
+  this[this.len] = value;
+
+}
+stackMethods.pop = function(){
+  var result;
+  if (this.len > 0) {
+    result = this[this.len];
+    delete this[this.len];
+    this.len--;
+  }
+  return result;
+}
+stackMethods.size = function(){
+  return this.len;
+}
 
