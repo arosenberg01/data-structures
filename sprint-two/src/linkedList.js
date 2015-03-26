e91ded96986694ecbcff5c8f2c656952c589235e
arosenberg01 <ansel01@gmail.com>
2015-03-26 15:51:09 -0700

    complete LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    node = Node(value);
+    if(!list.head) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    var result = list.head;
+    list.head = list.head.next;
+    return result.value;
   };
 
   list.contains = function(target){
+    var node = list.head;
+    while(node){
+      if (node.value === target) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
   };
 
   return list;
