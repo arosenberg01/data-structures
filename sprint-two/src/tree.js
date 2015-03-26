feba9a4c7365d926941eefdbf21e2563371263d0
arosenberg01 <ansel01@gmail.com>
2015-03-26 16:28:22 -0700

    complete tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,8 +3,9 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -15,11 +16,27 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var tree = Tree(value);
+  this.children.push(tree);
 };
 
 treeMethods.contains = function(target){
+  var currentTree = this;
+  var found = false;
 
+  var checkTree = function(tree) {
+    if (tree.value === target || found) {
+      found = true;
+    } else {
+      for (var i = 0; i < tree.children.length; i++) {
+        checkTree(tree.children[i]);
+      }
+    }
+  };
+
+  checkTree(currentTree);
+
+  return found;
 };
 
 
