f01dc55c15cc936c70fba4f525ba43bfb63e4264
arosenberg01 <ansel01@gmail.com>
2015-03-27 15:18:49 -0700

    complete hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,39 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if(!this._storage[i]){
+    this._storage[i] = [];
+  }
+  var keyValPair = [];
+  keyValPair.push(k);
+  keyValPair.push(v);
+  this._storage[i].push(keyValPair);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage[i]
+  var result = null;
+  for(var j = 0; j < bucket.length; j++){
+    if(bucket[j][0] === k){
+      result = bucket[j][1];
+      break;
+    }
+  }
+  return result;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage[i];
+  var result = null;
+  for(var j = 0; j < bucket.length; j++){
+    if(bucket[j][0] === k){
+      result = bucket[j][1];
+      bucket[j][0] = null;
+    }
+  }
+  return result;
 };
 
 
