023d42efbfc77a2f634bd808fc9772744967ae5a
arosenberg01 <ansel01@gmail.com>
2015-03-27 16:27:51 -0700

    Complete Part 2

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,64 @@
 var BinarySearchTree = function(value){
+  var obj = {};
+  obj.left;
+  obj.right;
+  obj.value = value;
+  _.extend(obj, BinarySearchTreeMethods);
+
+  return obj;
+};
+
+var BinarySearchTreeMethods = {};
+
+BinarySearchTreeMethods.insert = function(v){
+  if(this.value !== v){
+    if(v > this.value){
+      if(this.right){
+        this.right.insert(v);
+      } else {
+        this.right = BinarySearchTree(v);
+      }
+    } else {
+      if(this.left){
+        this.left.insert(v);
+      } else {
+        this.left = BinarySearchTree(v);
+      }
+    }
+  }
+};
+
+BinarySearchTreeMethods.contains = function(v){
+      debugger;
+  if(this.value === v){
+    return true;
+  } else if(v > this.value){
+    if(this.right){
+      return this.right.contains(v);
+    } else {
+      return false;
+    }
+  } else {
+    if(this.left){
+      return this.left.contains(v);
+    } else {
+      return false;
+    }
+  }
+};
+
+BinarySearchTreeMethods.depthFirstLog = function(cb){
+  cb(this.value);
+  if(this.left){
+    this.left.depthFirstLog(cb);
+  } else if(this.right){
+    this.right.depthFirstLog(cb);
+  }
+  // call callback on this.value
+  // if this.left exists
+    // depthFirstLog this.left
+  // else if this.right exists
+    // depthFirstLog this.right
 };
 
 
