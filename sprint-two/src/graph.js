567d1fa7bbadf8e4e508a28948c0754ca831cbf3
arosenberg01 <ansel01@gmail.com>
2015-03-27 11:03:33 -0700

    complete graph up to forEachNode

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,24 +1,40 @@
 
 
 var Graph = function(){
+  this.vertices = {};
 };
 
 Graph.prototype.addNode = function(node){
+  this.vertices[node] = {};
 };
 
 Graph.prototype.contains = function(node){
+  return !!this.vertices[node];
 };
 
 Graph.prototype.removeNode = function(node){
+  if(this.vertices[node]){
+    delete this.vertices[node];
+  }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  debugger;
+  return !!this.vertices[fromNode][toNode] && !!this.vertices[toNode][fromNode]
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  if(this.vertices[fromNode] && this.vertices[toNode] && this.vertices[toNode] !== this.vertices[fromNode]){
+    this.vertices[fromNode][toNode] = fromNode;
+    this.vertices[toNode][fromNode] = fromNode;
+  }
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  if(this.vertices[fromNode][toNode] && this.vertices[toNode][fromNode]){
+    delete this.vertices[fromNode][toNode];
+    delete this.vertices[toNode][fromNode];
+  }
 };
 
 Graph.prototype.forEachNode = function(cb){
